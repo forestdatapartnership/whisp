619a32a2c4ba4bf8275250a12b8b090e08db0f70
andyarnell <you@example.com>
2023-12-05 19:02:48 +0000

    more putting into modules etc

--- parameters/.ipynb_checkpoints/config_radd_alerts-checkpoint.py
+++ parameters/.ipynb_checkpoints/config_radd_alerts-checkpoint.py
@@ -5,4 +5,5 @@ local_alerts_buffer_radius = 2000
 # Define how many days back for alerts
 how_many_days_back = -(365*2)  # must be negative
 
-
+#max error in buffer (coarser buffers are quicker)
+max_error_alert_buff = 100
