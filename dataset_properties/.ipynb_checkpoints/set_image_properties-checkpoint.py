d3bc3f3b98a89d6c51ab3a53c21c30b568d9a567
andyarnell <you@example.com>
2023-12-06 16:40:39 +0000

    tidying

--- dataset_properties/.ipynb_checkpoints/set_image_properties-checkpoint.py
+++ dataset_properties/.ipynb_checkpoints/set_image_properties-checkpoint.py
@@ -1,5 +1,4 @@
 # ### Create image collection and set properties 
-# - set special properties for images 
 # - create image collection
 # - name of image is added 
 # - in preparation for reduceRegions (i.e., zonal) statistics 
@@ -22,22 +21,6 @@ if debug: print ("Starting image prep. This can take a minute")
 st = time.time()
 
 from datasets import *
-
-#deforestation alerts
-# set property so run stats for a buffer around site; 
-# and show presence only as output 
-latest_radd_alert_confirmed_recent_area_hectares = latest_radd_alert_confirmed_recent_area_hectares.setMulti(
-    {"alerts_buffer":1,"presence_only_flag":1})
-
-#important sites: 1) protected areas and 2) KBAs (likely future protectred areas) 
-# show presence only as output 
-protected_areas_WDPA_area_hectares = protected_areas_WDPA_area_hectares.set("presence_only_flag",1)
-
-OECM_2023_area_hectares = OECM_2023_area_hectares.set("presence_only_flag",1)
-
-kba_2023_area_hectares = kba_2023_area_hectares.set("presence_only_flag",1)
-
-print (kba_2023_area_hectares.get("presence_only_flag").getInfo())
 
 # turn into an image collection
 
