b4174bf3c43351ca33ef7f1b8a1fb84f1db6a211
andyarnell <you@example.com>
2023-12-07 22:35:00 +0000

    improved export fucntionality and exporting jrc 2020 data

--- parameters/config_runtime.py
+++ parameters/config_runtime.py
@@ -1,6 +1,6 @@
 ### general runtime parameters
 
-use_existing_image_collection = False  # use existing image collection (faster if exists), or create one on the fly in GEE (takes longer) (True or False)
+use_existing_image_collection = True  # use existing image collection (faster if exists), or create one on the fly in GEE (takes longer) (True or False)
 
 debug = True  # get print messages or not (e.g. for debugging code etc) (True or False)
 
