fbc6de39324202c173b53b5b748fb2acd21a55d0
andyarnell <you@example.com>
2023-12-07 17:01:34 +0000

    adding placeholder for JRC GFC 2020 data

--- datasets/jrc_tmf_disturbed.py
+++ datasets/jrc_tmf_disturbed.py
@@ -17,5 +17,5 @@ JRC_TMF_disturbed_2020 = JRC_TMF_transitions_remap.eq(2)
 JRC_TMF_disturbed_2020_area_hectares  = area_stats.binary_to_area_hectares(JRC_TMF_disturbed_2020)
 
 JRC_TMF_disturbed_2020_area_hectares = area_stats.set_scale_property_from_image(JRC_TMF_disturbed_2020_area_hectares,
-                                                                  JRC_TMF_transitions_raw.first(),debug=True).set("dataset_id",dataset_id)
+                                                        JRC_TMF_transitions_raw.first(),debug=True).set("dataset_id",dataset_id)
                                      
