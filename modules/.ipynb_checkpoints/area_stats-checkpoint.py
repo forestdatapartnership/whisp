5ac0d87f8fc0bf5d94d071eddcdeffe4fdf20aae
andyarnell <you@example.com>
2023-12-07 16:53:35 +0000

    feature creation and processing lists etc tidied away

--- modules/.ipynb_checkpoints/area_stats-checkpoint.py
+++ modules/.ipynb_checkpoints/area_stats-checkpoint.py
@@ -54,3 +54,34 @@ def add_area_hectares_property_to_feature_collection(fc,geometry_area_column):
 #     # if debug:
 #     #     print(feature.get(geometry_area_column).getInfo())
 #     return feature
+
+def make_processing_lists_from_gee_datasets_lookup(lookup_gee_datasets):
+    # global all_dataset_list
+    global local_buffer_stats_list
+    global flag_list
+    global country_allocation_stats_only_list
+    global normal_poly_stats_list
+    global decimal_place_column_list
+    
+    all_dataset_list = list(lookup_gee_datasets["dataset_name"])
+    
+    local_buffer_stats_list = list(lookup_gee_datasets["dataset_name"][(lookup_gee_datasets["local_buffer"]==1)])
+    
+    flag_list = list(lookup_gee_datasets["dataset_name"][(lookup_gee_datasets["presence_only_flag"]==1)])
+    
+    country_allocation_stats_only_list = list(lookup_gee_datasets["dataset_name"]     
+                                              [(lookup_gee_datasets["country_allocation_stats_only"]==1)])
+    
+    # for regular stat calculation in ploygon 
+    # remove country stats (as modal only) and remove buffer stats (assuming buffer only - NB this may change)
+    normal_poly_stats_list = [i for i in all_dataset_list if i not in 
+                              country_allocation_stats_only_list+local_buffer_stats_list] 
+    
+    decimal_place_column_list =  [i for i in all_dataset_list if i not in flag_list + country_allocation_stats_only_list]
+    
+    return 
+    local_buffer_stats_list, 
+    flag_list, 
+    country_allocation_stats_only_list, 
+    normal_poly_stats_list, 
+    decimal_place_column_list
