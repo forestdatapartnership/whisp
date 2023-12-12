d160ae2b2d94188b7b1a9a746be5a2d1f045e81f
andyarnell <you@example.com>
2023-12-12 13:19:19 +0000

    all cahnges from backup: datasets from functions; image collection sorting from lookup etc

--- datasets/glad_gfc_10pc.py
+++ datasets/glad_gfc_10pc.py
@@ -1,24 +1,31 @@
 import os
 import ee
 
-import modules.image_prep as image_prep
-import modules.area_stats as area_stats
+# import modules.image_prep as image_prep
+# import modules.area_stats as area_stats
 
-from datasets.glad_gfc_raw import gfc
+# from datasets.glad_gfc_raw import gfc
 
 dataset_id = 1
 
 ee.Initialize()
 
+def glad_gfc_10pc_prep(dataset_id):
+    
+    import modules.area_stats as area_stats
+    # gfc = ee.Image("UMD/hansen/global_forest_change_2022_v1_10")
+    
+    from datasets.glad_gfc_raw import gfc # so use a common asset
+    
+    gfc_treecover_2000 = gfc.select(['treecover2000']) #get tree cover in 2000
 
-gfc_treecover_2000 = gfc.select(['treecover2000']) #get tree cover in 2000
+    gfc_loss_2001_2020 = gfc.select(['lossyear']).lte(20) # get loss pixels since 2000 and up to and including 2020
 
-gfc_loss_2001_2020 = gfc.select(['lossyear']).lte(20) # get loss pixels since 2000 and up to and including 2020
+    gfc_treecover_2020 = gfc_treecover_2000.where(gfc_loss_2001_2020.eq(1),0) # remove loss from original tree cover
 
-gfc_treecover_2020 = gfc_treecover_2000.where(gfc_loss_2001_2020.eq(1),0) # remove loss from original tree cover
-
-gfc_treecover_2020_binary= gfc_treecover_2020.gt(10) #FAO 10% definition...
-
-gfc_treecover_2020_area_hectares = area_stats.binary_to_area_hectares(gfc_treecover_2020_binary)
-
-gfc_treecover_2020_area_hectares = area_stats.set_scale_property_from_image(gfc_treecover_2020_area_hectares,gfc,debug=True).set("dataset_id",dataset_id)
+    gfc_treecover_2020_binary= gfc_treecover_2020.gt(10) #FAO 10% definition...
+    
+    gfc_treecover_2020_binary = area_stats.set_scale_property_from_image(gfc_treecover_2020_binary,gfc,debug=True)
+    output_image = gfc_treecover_2020_binary
+    
+    return output_image.set("dataset_id",dataset_id)
--- datasets/jrc_tmf_undisturbed.py
+++ datasets/jrc_tmf_undisturbed.py
@@ -1,20 +1,28 @@
 import os
 import ee
 
-import modules.image_prep as image_prep
-import modules.area_stats as area_stats
+# # import modules.image_prep as image_prep
+# # import modules.area_stats as area_stats
 
-from datasets.jrc_tmf_prep import JRC_TMF_transitions_remap,JRC_TMF_transitions_raw
+# # from datasets.jrc_tmf_reclassify import JRC_TMF_transitions_remap,JRC_TMF_transitions_raw
 
 ee.Initialize()
 
-dataset_id = 5
+# dataset_id = 5
 
-## 1) undisturbed forest, 2) disturbed forest and 3) plantation
-JRC_TMF_undisturbed_2020 = JRC_TMF_transitions_remap.eq(1)
+def jrc_tmf_undisturbed_prep(dataset_id):
+    import modules.area_stats as area_stats
+#     from datasets.jrc_tmf_reclassify import jrc_tmf_prep
+      
+#     ##recoded values for tmf: 1) undisturbed forest, 2) disturbed forest and 3) plantation           
+#     JRC_TMF_transitions_remap = jrc_tmf_prep(ee.ImageCollection('projects/JRC/TMF/v1_2021/TransitionMap_Subtypes'))
+    from datasets.jrc_tmf_reclassify import JRC_TMF_transitions_remap,JRC_TMF_transitions_raw
+    
+    JRC_TMF_undisturbed_2020 = JRC_TMF_transitions_remap.eq(1) # select undisturbed tropical moist forest
 
-JRC_TMF_undisturbed_2020_area_hectares =area_stats.binary_to_area_hectares(JRC_TMF_undisturbed_2020)
+    JRC_TMF_undisturbed_2020 = area_stats.set_scale_property_from_image(
+        JRC_TMF_undisturbed_2020,JRC_TMF_transitions_raw.first(),debug=True)
 
-JRC_TMF_undisturbed_2020_area_hectares = area_stats.set_scale_property_from_image(JRC_TMF_undisturbed_2020_area_hectares,
-                                                                  JRC_TMF_transitions_raw.first(),debug=True).set("dataset_id",dataset_id).set("dataset_id",dataset_id)
-                                     
+    output_image = JRC_TMF_undisturbed_2020
+    
+    return output_image.set("dataset_id",dataset_id)
