5f243c7701b424a71567817cfc3d38e14ed10d1c
jonnvega <47796239+jonnvega@users.noreply.github.com>
2023-12-19 20:53:22 +0100

    delete system files

--- .ipynb_checkpoints/README-checkpoint.md
+++
@@ -1,2 +0,0 @@
-# fdap
-Forest Data Partnership notebooks
--- modules/.ipynb_checkpoints/json_to_ee-checkpoint.py
+++
@@ -1,4 +0,0 @@
-
-def json_to_feature_with_id(poly_json,geo_id,geo_id_column):
-    """converts json into a feature with a specified id column"""
-    return ee.Feature(ee.Geometry.Polygon(poly_json),ee.Dictionary([geo_id_column,geo_id]))
