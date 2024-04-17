6585ebbd286341c2eb2239efffc5ff47a5298590
andyarnell <you@example.com>
2024-04-17 16:40:12 +0000

    various: checks for lookup; datasets tidier

--- parameters/config_runtime.py
+++ parameters/config_runtime.py
@@ -5,6 +5,8 @@ ee.Initialize()
 
 debug = True  # get print messages or not (e.g. for debugging code etc) (True or False)
 
+# output_units = "percent" # "percent" or "ha" 
+
 out_path = os.path.join('/home/sepal-user/whisp/')
 
 #wide csv (main output format)
@@ -15,7 +17,6 @@ out_file_wide = out_path+out_wide_csv_name #set full path for output csv
 out_shapefile_name = "shapefile_for_ceo.shp.zip"
 
 out_shapefile = out_path + out_shapefile_name
-
 
 #output column names 
 geometry_area_column = "Area_ha"
@@ -29,3 +30,8 @@ plot_id_column = "PLOTID"
 path_lookup_gee_datasets_df = "parameters/lookup_gee_datasets.csv"
 
 lookup_gee_datasets_df = pd.read_csv(path_lookup_gee_datasets_df)
+
+#for stroing backup/temp csv files
+temp_csvs_folder_name = "backup_csvs"
+
+temp_csvs_folder_path = out_path + temp_csvs_folder_name
