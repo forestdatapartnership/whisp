4c545072919a9e71f66db291c628f637dd292eed
andyarnell <you@example.com>
2024-05-27 14:22:52 +0000

    small changes - tweaks to readme and others

--- parameters/config_runtime.py
+++ parameters/config_runtime.py
@@ -7,12 +7,15 @@ debug = True  # get print messages or not (e.g. for debugging code etc) (True or
 
 from parameters.config_directory import RESULTS_DIR,BACKUP_CSVS_DIR 
 
-#unit choice ("ha" or "percent")
-percent_or_ha = "ha"  
+
+## unit choice ("ha" or "percent")
+percent_or_ha = "percent"  
 
 out_directory = RESULTS_DIR
 
-#wide csv (main output format)
+
+
+### main outputs (main output format)
 out_csv_name = 'whisp_output_table.csv' #set output name
 
 out_csv = out_directory / out_csv_name #set full path for output csv
@@ -20,6 +23,7 @@ out_csv = out_directory / out_csv_name #set full path for output csv
 out_shapefile_name = "shapefile_for_ceo.shp.zip"
 
 out_shapefile = out_directory / out_shapefile_name
+
 
 
 ### whisp outputs formatting
@@ -53,14 +57,18 @@ stats_percent_columns_formatting = '%.0f'
 # ordering prefix columns: inserted before stats columns (plot metadata and stats unit type). 
 prefix_columns_list =[geo_id_column, geometry_area_column, geometry_type_column, country_column, centroid_x_coord_column, centroid_y_coord_column, stats_unit_type_column] 
 
+
 #do you want to keep system:index from input feature collection? NB it's useful for making joins after processing
 keep_system_index = True
+
 
 # do you keep other properties from input feature collection?
 keep_original_properties = False
 
+
 #lookup path - for dataset info
 path_lookup_gee_datasets_df = "parameters/lookup_gee_datasets.csv"
+
 
 lookup_gee_datasets_df = pd.read_csv(path_lookup_gee_datasets_df)
 
